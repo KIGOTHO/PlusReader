65db6edd9fb3a2b7185a1aa4c6523c751265f407
anthonymiyoro <amiyoro@gmail.com>
2015-07-30 18:37:56 +0300

    Added GET and SET methods for the RSSItem class

--- app/src/androidTest/java/com/example/tony/plusreader/RSSItem.java
+++ app/src/androidTest/java/com/example/tony/plusreader/RSSItem.java
@@ -24,4 +24,51 @@ public class RSSItem {
         this._pubdate = pubdate;
         this._guid = guid;
     }
+
+    /**
+     * SET methods that handles the users input from RSSfeed class
+     * */
+    public void setTitle(String title){
+        this._title = title;
+    }
+
+    public void setLink(String link){
+        this._link = link;
+    }
+
+    public void setDescription(String description){
+        this._description = description;
+    }
+
+    public void setPubdate(String pubDate){
+        this._pubdate = pubDate;
+    }
+
+
+    public void setGuid(String guid){
+        this._guid = guid;
+    }
+
+    /**
+     * GET method that handles input from the RSS xml file
+     * */
+    public String getTitle(){
+        return this._title;
+    }
+
+    public String getLink(){
+        return this._link;
+    }
+
+    public String getDescription(){
+        return this._description;
+    }
+
+    public String getPubdate(){
+        return this._pubdate;
+    }
+
+    public String getGuid(){
+        return this._guid;
+    }
 }
