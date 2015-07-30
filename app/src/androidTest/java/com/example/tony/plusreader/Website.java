e3e5f680a8de2e795cb9d746a87b1357df311043
anthonymiyoro <amiyoro@gmail.com>
2015-07-30 18:50:35 +0300

    Added variables and constructor for the Website class

---
+++ app/src/androidTest/java/com/example/tony/plusreader/Website.java
@@ -0,0 +1,19 @@
+package com.example.tony.plusreader;
+
+/**
+ * Handles the data from the SQLite  database
+ */
+public class Website {
+//    declares the variables used by the RSS file
+    Integer _id;
+    String _title;
+    String _link;
+    String _rss_link;
+    String _description;
+
+    // constructor (duplicates multiple website objects)
+    public Website(){
+
+    }
+
+}
