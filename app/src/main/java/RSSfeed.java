9860f404ded9e9e27e81b0235e764254dae133db
anthonymiyoro <amiyoro@gmail.com>
2015-07-30 18:08:16 +0300

    Added descriptions to class file

--- app/src/main/java/RSSfeed.java
+++ app/src/main/java/RSSfeed.java
@@ -11,3 +11,13 @@ public class RSSfeed {
     String _language;
     List<RSSItem> _items;
 }
+
+    // Assigns the descriptions of the RSS feeds
+    public RSSfeed ( String title, String description, String link,
+                   String rss_link, String language) {
+        this._title = title;
+        this._description = description;
+        this._link = link;
+        this._rss_link = rss_link;
+        this._language = language;
+    }
