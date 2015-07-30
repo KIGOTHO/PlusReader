3a1555b16e5baa9d7d60f0a614911a7c0b9805d4
anthonymiyoro <amiyoro@gmail.com>
2015-07-30 18:58:01 +0300

    Added SET and GET methods

--- app/src/androidTest/java/com/example/tony/plusreader/Website.java
+++ app/src/androidTest/java/com/example/tony/plusreader/Website.java
@@ -24,4 +24,50 @@ public class Website {
         this._rss_link = rss_link;
         this._description = description;
     }
+
+    /**
+     * Added set methods
+     * */
+    public void setId(Integer id){
+        this._id = id;
+    }
+
+    public void setTitle(String title){
+        this._title = title;
+    }
+
+    public void setLink(String link){
+        this._link = link;
+    }
+
+    public void setRSSLink(String rss_link){
+        this._rss_link = rss_link;
+    }
+
+    public void setDescription(String description){
+        this._description = description;
+    }
+
+    /**
+     * Added get methods
+     * */
+    public Integer getId(){
+        return this._id;
+    }
+
+    public String getTitle(){
+        return this._title;
+    }
+
+    public String getLink(){
+        return this._link;
+    }
+
+    public String getRSSLink(){
+        return this._rss_link;
+    }
+
+    public String getDescription(){
+        return this._description;
+    }
 }
