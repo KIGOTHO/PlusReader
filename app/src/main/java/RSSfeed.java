6d5def459bc19c5df2ff876338697d398d7392e2
anthonymiyoro <amiyoro@gmail.com>
2015-07-30 18:04:28 +0300

    Added dependencies to class file

--- app/src/main/java/RSSfeed.java
+++ app/src/main/java/RSSfeed.java
@@ -1,10 +1,13 @@
-/**
- * Created by tony on 30/07/15.
- */
-
+import java.util.List;
 /**
  * This class handle rss xml
  * */
 public class RSSfeed {
-
+    // list the things to expect from the feed
+    String _title;
+    String _description;
+    String _link;
+    String _rss_link;
+    String _language;
+    List<RSSItem> _items;
 }
